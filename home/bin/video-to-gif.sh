1a27f8da1bad087fb2fce56a2c542ff218852b93
Patrick Collins <pat@burned.com>
2020-04-24 17:27:28 -0400

    update video to gif script

--- home/bin/video-to-gif.sh
+++ home/bin/video-to-gif.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Need ffmpeg, imagemagick, gifsicle
 if [[ $1 == "" ]]; then
   echo please pass video file as first argument
   exit 1
@@ -9,5 +10,4 @@ video_file_basename=`basename ${video_file}`
 tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 echo 'tmpdir'
 echo $tmpdir
-ffmpeg -i "$video_file" -vf fps=1 $tmpdir/${video_file_basename}_%03d.jpg
-convert $tmpdir/${video_file_basename}_*.jpg -resize 50% GIF:- | gifsicle --delay=50 --loop --optimize=2 --colors=256 --multifile - > ${video_file}.gif
+ffmpeg -i "$video_file" -vf "scale=iw/3:ih/3" -pix_fmt rgb8 -r 5 -f gif - | gifsicle --optimize=3 > ${video_file}.gif
