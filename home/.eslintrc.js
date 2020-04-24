fd4d7854aa46477a8a981884409421dd5c11b18e
Patrick Collins <pat@burned.com>
2020-04-24 17:21:42 -0400

    Add default JS linting

---
+++ home/.eslintrc.js
@@ -0,0 +1,19 @@
+// Default ESLint config for Node.
+// Created mostly to make ALE happy.
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
