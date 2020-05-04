431848dc3a0c4006a2a24a39acd36671a30f2304
Patrick Collins <pat@burned.com>
2020-05-04 15:26:09 -0400

    Commit xbindkeys config

---
+++ home/.xbindkeysrc.scm
@@ -0,0 +1,5 @@
+; NOTE: add `xbindkeys` to `.xinitrc`
+
+; bind shift + vertical scroll to horizontal scroll events
+(xbindkey '(shift "b:4") "xte 'mouseclick 6'")
+(xbindkey '(shift "b:5") "xte 'mouseclick 7'")
