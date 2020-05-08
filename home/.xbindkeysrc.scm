46e3b84f84f0caf6b76ad9edf27d428282ce9e7a
Patrick Collins <pat@burned.com>
2020-05-08 09:46:51 -0400

    xbindkeys

--- home/.xbindkeysrc.scm
+++ home/.xbindkeysrc.scm
@@ -3,3 +3,5 @@
 ; bind shift + vertical scroll to horizontal scroll events
 (xbindkey '(shift "b:4") "xte 'mouseclick 6'")
 (xbindkey '(shift "b:5") "xte 'mouseclick 7'")
+
+; (xbindkey '(mod4 "c:25") "xte 'keydown Control_L' 'key w' 'keyup Control_L'")
