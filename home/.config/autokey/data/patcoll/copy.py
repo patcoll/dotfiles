13e441cbb9db6d12ff0d36b0de22404746ae5b77
Patrick Collins <pat@burned.com>
2020-05-09 17:06:57 -0400

    autokey updates
    disable for now

--- home/.config/autokey/data/patcoll/copy.py
+++ home/.config/autokey/data/patcoll/copy.py
@@ -1,9 +1,9 @@
 import re
 
-store.set_global_value('hotkey', '<alt>+c')
+store.set_global_value('hotkey', '<shift>+<ctrl>+<alt>+c')
 
-if re.match('.*(Emacs|Guake|Gnome-terminal|Gvim|Eclipse|io.elementary.terminal)', window.get_active_class()):
-    engine.set_return_value('<ctrl>+<shift>+c')
+if re.match('.*(Emacs|Guake|Gnome-terminal|Gvim|Eclipse|io.elementary.terminal|kitty).*', window.get_active_class()):
+    engine.set_return_value('<shift>+<ctrl>+c')
 else:
     engine.set_return_value('<ctrl>+c')
 
--- home/.config/autokey/data/patcoll/paste.py
+++ home/.config/autokey/data/patcoll/paste.py
@@ -1,10 +1,9 @@
 import re
 
-store.set_global_value('hotkey', '<ctrl>+v')
+store.set_global_value('hotkey', '<shift>+<ctrl>+<alt>+v')
 
-#engine.set_return_value('<ctrl>+v')
 if re.match('.*(Emacs|Guake|Gnome-terminal|Gvim|Eclipse|io.elementary.terminal|kitty).*', window.get_active_class()):
-    engine.set_return_value('<ctrl>+<shift>+v')
+    engine.set_return_value('<shift>+<ctrl>+v')
 else:
     engine.set_return_value('<ctrl>+v')
 
