a4bc01037ae4a7bdb68979882c2888473a00f28d
Patrick Collins <pat@burned.com>
2020-05-10 14:48:19 -0400

    shortcuts coming in for a landing
    - xkb overrides:
      `['terminate:ctrl_alt_bksp', 'ctrl:nocaps', 'altwin:swap_alt_win', 'local']`
    - local overrides make `super+[A-Za-z0-9]` plus basic symbols map to
      ctrl instead of super
    - swap out / add some more mac-like behavior with autokey
    - update kitty config to match
    - add `custom-keymap` script that can be run like this:
      `custom-keymap | xkbcomp -w3 - $DISPLAY`
    - update `dconf/user.txt` flat-file-sync'd format for backup

--- home/.config/autokey/data/patcoll/tab-next.py
+++ home/.config/autokey/data/patcoll/tab-next.py
@@ -1,10 +1,12 @@
 import re
 
-store.set_global_value('hotkey', '<alt>+<ctrl>+]')
+store.set_global_value('hotkey', '<super>+]')
 
 if re.match('.*(Chromium|Firefox)', window.get_active_class()):
     engine.set_return_value('<ctrl>+<page_down>')
+elif re.match('.*(Slack).*', window.get_active_class()):
+    engine.set_return_value('<alt>+<right>')
 else:
-    engine.set_return_value('<alt>+<ctrl>+]')
+    engine.set_return_value('<super>+]')
 
 engine.run_script('combo')
--- home/.config/autokey/data/patcoll/tab-prev.py
+++ home/.config/autokey/data/patcoll/tab-prev.py
@@ -1,10 +1,12 @@
 import re
 
-store.set_global_value('hotkey', '<alt>+<ctrl>+[')
+store.set_global_value('hotkey', '<super>+[')
 
 if re.match('.*(Chromium|Firefox)', window.get_active_class()):
     engine.set_return_value('<ctrl>+<page_up>')
+elif re.match('.*(Slack).*', window.get_active_class()):
+    engine.set_return_value('<alt>+<left>')
 else:
-    engine.set_return_value('<alt>+<ctrl>+[')
+    engine.set_return_value('<super>+[')
 
 engine.run_script('combo')
