f804b89a02c382f44ddeb596356c98e681164590
Patrick Collins <pat@burned.com>
2020-09-02 12:19:22 -0400

    autokey updates

--- home/.config/autokey/data/patcoll/tab-next.py
+++ home/.config/autokey/data/patcoll/tab-next.py
@@ -2,7 +2,5 @@ app = window.get_active_class()
 
 if 'Chromium' in app or 'Firefox' in app:
     keyboard.send_keys('<ctrl>+<page_down>')
-elif 'Slack' in app:
-    keyboard.send_keys('<alt>+<right>')
 else:
     keyboard.send_keys('<shift>+<ctrl>+]')
--- home/.config/autokey/data/patcoll/tab-prev.py
+++ home/.config/autokey/data/patcoll/tab-prev.py
@@ -2,7 +2,5 @@ app = window.get_active_class()
 
 if 'Chromium' in app or 'Firefox' in app:
     keyboard.send_keys('<ctrl>+<page_up>')
-elif 'Slack' in app:
-    keyboard.send_keys('<alt>+<left>')
 else:
     keyboard.send_keys('<shift>+<ctrl>+[')
